af2bff9253181211acf721fa919a14fa6bedfd70
hesong <actkhs@gmail.com>
2020-03-28 16:10:49 +0800

    Add react frontend module

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <Fragment className="App">
+      <h1>App</h1>
+    </Fragment>
+  );
+};
+
+export default App;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // connect database
 connectDB();
 
+// Init middleware, allow us to get data from req.body
+app.use(express.json({ extended: false }));
+
 app.get("/", (req, res) => res.send("API running..."));
 
 // Define routes
@@ -13,6 +16,18 @@ app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 
+// Serve statics assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+
+module.exports = app;
