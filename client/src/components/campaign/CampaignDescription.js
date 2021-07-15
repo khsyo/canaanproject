c5e980fa3063d67bb25f32d6d663ea21a1bb49c5
hesong101 <53859695+hesong101@users.noreply.github.com>
2021-07-15 17:32:07 +0800

    Change hero-banner and copy text

--- client/src/components/campaign/CampaignDescription.js
+++ client/src/components/campaign/CampaignDescription.js
@@ -4,12 +4,10 @@ const CampaignDescription = () => {
   return (
     <Fragment>
       <div className="description__wrapper">
-        <h2 className="description__title">2020 東南亞人才線上聯合招募</h2>
+        <h2 className="description__title">Global Talent 線上聯合招募</h2>
         <p className="description__paragraph">
           <span>The Canaan Project 一個專屬僑外生的求職社群平台</span>
-          <span>
-            首波連結台灣優質企業，提供從餐飲、行銷、設計到工程等熱門職缺
-          </span>
+          <span>協助企業找到優質的跨國人才</span>
           <span>歡迎應屆畢業生或是想要轉職的你一起來應徵</span>
           {/* <br />
           <span>一次 10 分鐘，輪流與各家老闆、主管面對面</span>
